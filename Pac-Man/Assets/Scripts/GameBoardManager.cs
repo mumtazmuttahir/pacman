344ffa56663396f7ce8ed6f1018243ab937e0754
Muttahir Mumtaz <muttahirmumtaz@muttahirs-mbp>
2021-02-25 02:04:01 +0100

    First Commit with the Pac-man moving.

---
+++ Pac-Man/Assets/Scripts/GameBoardManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoardManager : MonoBehaviour
+{
+
+    public static int boardWidth = 28;
+    public static int boardHeight = 36;
+
+    public GameObject[,] gameBoard = new GameObject[boardWidth, boardHeight];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object[] objects = GameObject.FindObjectsOfType(typeof(GameObject));
+
+        foreach (GameObject obj in objects) {
+            Vector2 objPos = obj.transform.position;
+
+            if (obj.gameObject.name != "Pacman") {
+                gameBoard[(int)objPos.x, (int)objPos.y] = obj;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
